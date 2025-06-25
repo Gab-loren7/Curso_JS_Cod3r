1a4fa38a2d88af2bf5b8fe3b5c88c628da1114c0
GABRIEL CARVALHO LORENSSETTI <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-06-25 09:49:15 -0300

    upload até factory#1

---
+++ Codigo do Curso/Javascript_Fundamentos/funcao/closure.js
@@ -0,0 +1,16 @@
+// Closure(fechamento) é o escopo criado quando uma fução é declarada
+// Esse escopo permite a função acessar e manipular variáveis externas à função
+
+// Contexto léxico em ação!
+
+const x = "Global";
+
+function fora(){
+    const x = "Local"
+    function dentro(){
+        return x
+    }; return dentro
+};
+
+const minhaFuncao = fora();
+console.log(minhaFuncao());
---
+++ Codigo do Curso/Javascript_Fundamentos/funcao/factory1.js
@@ -0,0 +1,11 @@
+// Factory retorna/fabrica um objeto
+
+// Factory simples
+function criarPessoa(){
+    return{
+        nome: "Ana",
+        sobrenome: "Silva"
+    };
+}
+
+console.log(criarPessoa());
