f62b19f9b266462ca38b4b51f246b3e86cbd27c1
Gabriel <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-07-07 09:27:41 -0300

    .git

--- Codigo do Curso/Javascript_Fundamentos/funcao/exerciciosJS_PDF/exercicio22.js
+++ Codigo do Curso/Javascript_Fundamentos/funcao/exerciciosJS_PDF/exercicio22.js
@@ -3,32 +3,13 @@ parâmetro um inteiro que representa o mês (1 - janeiro, 2 - fevereiro...) que 
 anuidade deve ser paga no mês de janeiro. Por mês, é cobrado 5% de juros (sob o regime de juros 
 compostos). O retorno deve ser o valor a ser pago para o respectivo mês escolhido. */
 
-const pagarAnuidade = (mesInteiro, valor) => {
-    switch (mesInteiro) {
-        case 1: 
-            return `A anuidade foi paga em 1 - Janeiro por: R$`
-        case 2:
-            return `A anuidade foi paga em 2 - Fevereiro por: R$`
-        case 3:
-            return `A anuidade foi paga em 3 - Março por: R$`
-        case 4:
-            return `A anuidade foi paga em 4 - Abril por: R$`
-        case 5:
-            return `A anuidade foi paga em 5 - Maio por: R$`
-        case 6:
-            return `A anuidade foi paga em 6 - Junho por: R$`
-        case 7:
-            return `A anuidade foi paga em 7 - Julho por: R$`
-        case 8:
-            return `A anuidade foi paga em 8 - Agosto por: R$`
-        case 9:
-            return `A anuidade foi paga em 9 - Setembro por: R$`
-        case 10:
-            return `A anuidade foi paga em 10 - Outubro por: R$`
-        case 11:
-            return `A anuidade foi paga em 11 - Novembro  por: R$`
-        case 12:
-            return `A anuidade foi paga em 12 - Dezembro por: R$`
-        default: return `Mês invalido para pagamento. Digite um número de 1 a 12`
+let calcularValor = (mes, valor) => {
+    if (mes > 0 && mes < 13) {
+        let atraso = mes - 1
+        return (valor * ((1 + (5 / 100)) ** atraso)).toFixed(2)
+    } else {
+        return `Mês inválido.`
     }
 }
+
+console.log(calcularValor(4, 100));
