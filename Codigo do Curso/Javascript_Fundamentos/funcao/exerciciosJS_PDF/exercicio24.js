c20a1b6a784a293538dfd056a21bebd449c0dc73
Gabriel <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-07-07 12:05:07 -0300

    upload até exercicio 25#

---
+++ Codigo do Curso/Javascript_Fundamentos/funcao/exerciciosJS_PDF/exercicio24.js
@@ -0,0 +1,8 @@
+/* Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.  */
+
+let num = 0
+
+while (num != 11) {
+    num ++
+    console.log("Hello World!"); 
+}
---
+++ Codigo do Curso/Javascript_Fundamentos/funcao/exerciciosJS_PDF/exercicio25.js
@@ -0,0 +1,9 @@
+/* Escrever um programa para exibir os números de 1 até 50 na tela. */
+
+let num = 0
+
+while (num != 50) {
+    num ++
+    console.log(`${num}#`);
+    
+}
